7279ff6dcffc53f8a7146522e0144962fce85770
sidney amajor <sidney-major@live.com.pt>
2020-07-20 18:37:41 +0100

    Init template

---
+++ Veterinary/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinary.Data
+{
+    public class DataContext:DbContext
+    {
+        //public DbSet<Client> Clients { get; set; }
+        //public DbSet<Animal> Animals { get; set; }
+        //public DbSet<Employee> Employees { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
